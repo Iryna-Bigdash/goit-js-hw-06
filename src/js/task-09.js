81dc200f86a622f2b592d981d5e744a97d7d57ce
Iryna B <imbigdash@gmail.com>
2023-02-24 12:35:38 +0000

    added 9 task

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
+const bodyEl = document.querySelector(".widget");
+const spanEl = document.querySelector(".color");
+const bntChangeEl = document.querySelector(".change-color");
+
+bntChangeEl.addEventListener("click", onClick);
+
+function onClick() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+};
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
