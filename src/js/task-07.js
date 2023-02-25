ba8ae6d13791295d74a4a79aefab821857a0ebd2
Iryna B <imbigdash@gmail.com>
2023-02-25 21:43:00 +0000

    added-10-task

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,9 +1,9 @@
 let refs = {
   inputEl: document.querySelector("#font-size-control"),
-  spanEl: document.querySelector('#text'),
+  spanEl: document.querySelector("#text"),
 };
-refs.inputEl.addEventListener('change', onChage);
+refs.inputEl.addEventListener("change", onChage);
 
 function onChage(event) {
   refs.spanEl.style.fontSize = Number(event.currentTarget.value) + "px";
-};
+}
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,43 @@
+const refs = {
+  inputEl: document.querySelector('[type="number"]'),
+  btnCreate: document.querySelector("[data-create]"),
+  btnDestroy: document.querySelector("[data-destroy]"),
+  boxEl: document.querySelector("#boxes"),
+};
+
+let amountInput = Number(refs.inputEl.value);
+
+function createBoxes(amount) {
+  let arrayOfDiv = [];
+
+  for (let i = 1; i <= amount; i += 1) {
+    let divItem = document.createElement("div");
+    divItem.style.width = `${20 + i * 10}px`;
+    divItem.style.height = `${20 + i * 10}px`;
+    divItem.style.backgroundColor = getRandomHexColor();
+    divItem.append(i);
+    arrayOfDiv.push(divItem);
+  }
+  refs.boxEl.append(...arrayOfDiv);
+}
+
+refs.btnCreate.addEventListener("click", onBtnCreate);
+function onBtnCreate() {
+  createBoxes(refs.inputEl.value);
+}
+
+function destroyBoxes() {
+  onBtnDestroy();
+}
+
+refs.btnDestroy.addEventListener("click", onBtnDestroy);
+function onBtnDestroy() {
+  refs.boxEl.remove();
+  refs.inputEl.value = "null";
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
